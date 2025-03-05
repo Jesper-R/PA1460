ce325b64c4f621b77461e62a455eb95a37076044
Jesper <Jesperrudegran@gmail.com>
2025-03-05 14:32:47 +0100

    unittest skeleton

--- testing/unit_tests.py
+++ testing/unit_tests.py
@@ -1,8 +1,17 @@
 import unittest
 
 class TestInteractWithObject(unittest.TestCase):
-    def test_1(self):
-        self.assertTrue(True)
+    def test_select_object(self):
+        pass
+
+    def test_select_interaction(self):
+        pass
+
+    def test_set_interaction_options(self):
+        pass
+
+    def test_start_interaction(self):
+        pass
 
 if __name__ == '__main__':
     unittest.main()

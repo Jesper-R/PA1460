140fedf19edc3f6483a3520992b766c7547474b4
Jesper <Jesperrudegran@gmail.com>
2025-03-05 14:48:04 +0100

    unit test

--- testing/unit_tests.py
+++ testing/unit_tests.py
@@ -3,23 +3,33 @@ import unittest
 class TestInteractWithObject(unittest.TestCase):
 
     def setUp(self):
+        print("- Setup -")
         pass
 
     def tearDown(self):
+        print("- Teardown -")
         pass
 
 
 
     def test_select_object(self):
+        print("Testing select_object")
+
         pass
 
     def test_select_interaction(self):
+        print("Testing select_interaction")
+
         pass
 
     def test_set_interaction_options(self):
+        print("Testing set_interaction_options")
+
         pass
 
     def test_start_interaction(self):
+        print("Testing start_interaction")
+
         pass
 
 if __name__ == '__main__':
